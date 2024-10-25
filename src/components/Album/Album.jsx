d3bd552eeb6a3698d7fa6f4582b78fb87adeac47
david.machio <david.machio@MB-07.local>
2024-10-25 16:52:41 +0200

    maquetacion albums finalizada

--- src/components/Album/Album.jsx
+++ src/components/Album/Album.jsx
@@ -7,9 +7,7 @@ import React, { memo } from 'react'
 const Album = memo(({album}) => {
   return (
     <article className='album-container'>
-      {console.log('renderizando albums')
-      }
-        <img src={album.cover} alt="Foto de la portada"  />
+        <img className='portada_del_album' src={album.cover} alt="Foto de la portada"  />
         <h2 className='album_title'>{album.title}</h2>
     </article>
   )
